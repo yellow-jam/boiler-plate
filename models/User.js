cf43a3f2c7e96bcefdc1d754b90bd7f20f386caa
unknown <lerle144@gmail.com>
2022-04-05 18:18:00 +0900

    10강 bcrypt로  암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +32,27 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+// 10강
+userSchema.pre('save', function( next ) {
+    var user = this;
+
+    // 비밀번호가 변경되었을 때에만 동작하도록 (다른 필드 X)
+    if (user.isModified('password')) { 
+        // 비밀번호를 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                // Store hash in your password DB.
+                user.password = hash    
+            });
+        });
+    }
+    next()
+})
+
 
 const User = mongoose.model('User', userSchema)
 
