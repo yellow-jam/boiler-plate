ee8b3c7d3984eb40b69b06794544fb454a8fd340
unknown <lerle144@gmail.com>
2022-04-12 21:05:37 +0900

    25강 Antd, 27강 redux & dependency 설치

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'antd/dist/antd.css';
 
 const container = document.getElementById('root');
 /*
